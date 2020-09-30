0b79b658e9d530f2b0505d171710a1930cb59b58
Jarudat Chaikuad <42264164+ucjim@users.noreply.github.com>
2020-10-01 02:55:13 +0700

    ml,,

---
+++ screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    Button,
+    StatusBar,
+  } from 'react-native';
+  
+  const HomeScreen = ({navigation}) => {
+    return (
+      <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Button 
+        title = " Go to details screen"
+        onPress={() => navigation.navigate("Details")} 
+        />
+      </View>
+    );   
+  };
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    },
+});
+  
+ 
