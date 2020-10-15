52a8369d6dd5000a0a2635a69e59b353dc23c6c6
Jarudat Chaikuad <42264164+ucjim@users.noreply.github.com>
2020-10-16 05:06:40 +0700

    Auth Firebase Authentication

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import MainTabScreen from './screens/MainTabScreen';
-
-
-const Drawer = createDrawerNavigator();
-
-
+import Providers from './navigation';
 
 const App = () => {
-  return(
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={MainTabScreen} />
-        {/*<Drawer.Screen name="SuppoetScreen" component={SupportScreen} />
-        <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
-  <Drawer.Screen name="SettingsScreen" component={BookmarkScreen} /> */}
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
-};
+  return <Providers />;
+}
 
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import {
-    SafeAreaView,
-    StyleSheet,
-    ScrollView,
-    View,
-    Text,
-    Button,
-    StatusBar,
-  } from 'react-native';
-  
-  const HomeScreen = ({navigation}) => {
-    return (
-      <View style={styles.container}>
-        <Text>Home Screen</Text>
-        <Button 
-        title = " Go to details screen"
-        onPress={() => navigation.navigate("Details")} 
-        />
-      </View>
-    );   
-  };
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import FormButton from '../components/FormButton';
+import { AuthContext } from '../navigation/AuthProvider';
+
+const HomeScreen = () => {
+  const {user, logout} = useContext(AuthContext);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome {user.uid}</Text>
+      <FormButton buttonTitle='Logout' onPress={() => logout()} />
+    </View>
+  );
+}
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        alignItems: 'center', 
-        justifyContent: 'center' 
-    },
+  container: {
+    backgroundColor: '#f9fafd',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 20,
+    color: '#333333'
+  }
 });
-  
- 
